0af56ca9064975cff2fad97f7063e7edfbc7a9a3
Gabe <Carrollgbusiness@gmail.com>
2026-05-19 11:15:26 -0500

    Build BankAccount & CreditCard Classes

--- src/main/java/com/pluralsight/finance/BankAccount.java
+++ src/main/java/com/pluralsight/finance/BankAccount.java
@@ -1,4 +1,52 @@
 package com.pluralsight.finance;
 
-public class BankAccount {
+import com.pluralsight.assets.Valuable;
+
+public class BankAccount implements Valuable {
+
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    //methods
+    //deposit(amount:double)
+    //withdraw(amount:double)
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+
+    //constructor
+    public BankAccount(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    //getter/setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
--- src/main/java/com/pluralsight/finance/CreditCard.java
+++ src/main/java/com/pluralsight/finance/CreditCard.java
@@ -1,4 +1,47 @@
 package com.pluralsight.finance;
 
-public class CreditCard {
+import com.pluralsight.assets.Valuable;
+
+public class CreditCard implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+
+    //constructor
+    public CreditCard(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    //getter/setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
